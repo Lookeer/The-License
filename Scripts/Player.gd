e3f2e354cf3e79f55f265cf693ed3a6b2bff1823
Lookeer <valentingames@yahoo.com>
2023-06-29 18:54:45 +0300

    Updated project files

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,12 +8,14 @@ var attack_state = 0
 
 var weapon : Weapon
 
-func _ready():
+func _ready() -> void:
 	Engine.time_scale = 1
-	$WeaponPivot/WeaponGrip/Sword.owner_entity = self
+	weapon = %WeaponGrip/Sword
+	%WeaponGrip/Sword.owner_entity = self
+	%WeaponGrip/Sword.animator = %WeaponAnimationTree
 	$AnimationTree["parameters/Walk/WalkSpeed/scale"] = SPEED / 100.0
 
-func _process(delta):
+func _process(delta) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		var lexer : Lexer = Lexer.new(%CodeEdit.text)
 		var arr := lexer.lex()
@@ -39,13 +41,13 @@ func _process(delta):
 #		get_node("../Enemy").set_process(true)
 #		get_node("../Enemy").set_physics_process(true)
 
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 	if freeze_input: return
 	get_input()
 	rotate_to_mouse()
 	move_and_slide()
 
-func get_input():
+func get_input() -> void:
 	var direction_x = Input.get_axis("move_left", "move_right")
 	var direction_y = Input.get_axis("move_up", "move_down")
 	var direction = Vector2(direction_x, direction_y)
@@ -57,17 +59,16 @@ func get_input():
 		velocity = Vector2.ZERO
 		$AnimationTree["parameters/conditions/walking"] = false
 		$AnimationTree["parameters/conditions/idling"] = true
-	if weapon && Input.is_action_just_pressed("attack"):
-		attack()
 
-func rotate_to_mouse():
-	$WeaponPivot.rotation = get_global_mouse_position().angle_to_point(position) - deg_to_rad(35)
+func rotate_to_mouse() -> void:
 	if get_global_mouse_position() > global_position:
 		$Sprite2D.flip_h = true
 		$WeaponPivot.scale.y = -1
 	else:
 		$Sprite2D.flip_h = false
 		$WeaponPivot.scale.y = 1
+	$WeaponPivot.rotation = get_global_mouse_position().angle_to_point(position)
 
-func attack():
-	weapon.attack()
+func toggle_weapon_hitbox(value : bool) -> void:
+	if !weapon: return
+	weapon.toggle_hitbox(value)
--- Scripts/Weapon.gd
+++ Scripts/Weapon.gd
@@ -1,41 +1,99 @@
 extends CharacterBody2D
 class_name Weapon
 
-var damage : float = 5
-var speed : float = 50
-var control : float = 200
-var max_battery : float = 100
+var attack_power_stat : float = 5
+var speed_stat : float = 50
+var control_stat : float = 50
+var max_battery_stat : float = 100
 
+var speed : float
+var control : float
 var battery : float
 
+enum Attack_State {SWING, SWING_BACK}
+var attack_state := Attack_State.SWING
 var held := true
 var moving := false
+var returning_to_owner := false
 var move_target : Vector2
 var owner_entity : Entity
+var animator : AnimationTree
 
-func _ready():
+func _ready() -> void:
 	$Hitbox.connect("body_entered", _on_body_entered)
-	battery = max_battery
+	speed = speed_stat * 10
+	control = control_stat / 500
+	battery = max_battery_stat
 
-func _process(delta):
+func _process(delta) -> void:
 	if Input.is_action_just_pressed("attack"):
 		attack()
+	if Input.is_action_just_pressed("hack_attack"):
+		handle_hack_attack()
 
-func _physics_process(delta):
-	if moving:
-		if global_position.distance_to(move_target) < velocity.length() * delta:
-			moving = false
-		else:
-			var rotation_angle = global_position.angle_to_point(move_target) + deg_to_rad(135)
-			rotation = lerp_angle(rotation, rotation_angle, delta * control / 10)
-			var forward = global_transform.y.rotated(deg_to_rad(135))
-			if global_position.direction_to(move_target).dot(forward) > 0.9999:
-				velocity = forward * speed * 10
+func _physics_process(delta) -> void:
+	if returning_to_owner:
+		return_to_owner(delta)
+	else:
+		handle_movement(delta)
 	move_and_slide()
-	velocity -= velocity * control / 1000
+	velocity -= velocity * control
 
 func attack() -> void:
-	pass
+	if returning_to_owner: return
+	if held:
+		swing()
+	else:
+		hack_attack()
+
+func handle_movement(delta : float) -> void:
+	if !moving: return
+	if global_position.distance_to(move_target) < velocity.length() * delta:
+		moving = false
+	else:
+		var rotation_angle = global_position.angle_to_point(move_target) + deg_to_rad(135)
+		rotation = lerp_angle(rotation, rotation_angle, delta * control_stat / 10)
+		var forward = global_transform.y.rotated(deg_to_rad(135))
+		if global_position.direction_to(move_target).dot(forward) > 0.9999:
+			velocity = forward * speed
+
+func return_to_owner(delta : float) -> void:
+	if global_position.distance_to(owner_entity.global_position) < velocity.length() * delta:
+		get_parent().remove_child(self)
+		owner_entity.get_node("%WeaponGrip").add_child(self)
+		rotation = 0
+		position = Vector2.ZERO
+		velocity = Vector2.ZERO
+		held = true
+		returning_to_owner = false
+		toggle_hitbox(false)
+		return
+	var direction = owner_entity.global_position - global_position
+	velocity = direction.normalized() * speed
+	rotation += 40 * delta
+
+func handle_hack_attack() -> void:
+	if held:
+		initialize_hack_attack()
+	else:
+		stop_hack_attack()
+
+func initialize_hack_attack() -> void:
+	var root = get_tree().current_scene
+	var current_pos = global_position
+	get_parent().remove_child(self)
+	root.add_child(self)
+	global_position = current_pos
+	held = false
+	animator["parameters/conditions/swinging"] = false
+	animator["parameters/conditions/swinging2"] = false
+	animator["parameters/conditions/reset"] = true
+	attack_state = Attack_State.SWING
+	toggle_hitbox(true)
+
+func stop_hack_attack() -> void:
+	moving = false
+	returning_to_owner = true
 
 func toggle_hitbox(value : bool) -> void:
 	$Hitbox.monitoring = value
@@ -45,9 +103,22 @@ func move(x : float, y : float) -> void:
 	moving = true
 
 func swing() -> void:
+	match attack_state:
+		Attack_State.SWING:
+			animator["parameters/conditions/reset"] = false
+			animator["parameters/conditions/swinging2"] = false
+			animator["parameters/conditions/swinging"] = true
+			attack_state = Attack_State.SWING_BACK
+		Attack_State.SWING_BACK:
+			animator["parameters/conditions/swinging"] = false
+			animator["parameters/conditions/swinging2"] = true
+			attack_state = Attack_State.SWING
+
+func hack_attack() -> void:
 	pass
 
 func _on_body_entered(body) -> void:
 	if body == owner_entity: return
+	var damager = owner_entity if held else self
 	if body.has_method("take_damage"):
-		body.take_damage(damage, self)
+		body.take_damage(attack_power_stat, damager)
